d11e360bd6acb50370224c84fddde098508eb7f9
Marius Göcke <marius.goecke@gmail.com>
2025-04-14 23:27:48 +0200

    added some overhead

---
+++ PtN/Other/QualityCheck/Linting.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+
+
+def linting():
+    pass  # TODO
+
+
+if __name__ == "__main__":
+    linting()
--- PtN/Other/QualityCheck/RunTestcases.py
+++ PtN/Other/QualityCheck/RunTestcases.py
@@ -1,2 +1,13 @@
 # go install github.com/t-yuki/gocover-cobertura@latest
 # gocover-cobertura < coverage.out > coverage.xml
+import sys
+from pathlib import Path
+from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+
+
+def run_testcases():
+    pass  # TODO
+
+
+if __name__ == "__main__":
+    run_testcases()
